46907e36886d5460ecc79e72fbe2ec4b5ad526ef
Prink0054 <turna.prink@gmail.com>
2018-08-22 15:02:55 +0530

    Data-Structure

--- Link-List/Implementation of Singly Linklist.py
+++ Link-List/Implementation of Singly Linklist.py
@@ -1,6 +1,6 @@
 # ========================Singly Linklist Implementation===================
 
-
+class Node:
     def __init__(self, value):
         self.value = value
         self.nextnode = None
