c7d6a669ebca4caf0a8543c19a91bd62a0bc1c9a
Prink0054 <turna.prink@gmail.com>
2018-08-22 14:39:11 +0530

    Insertion of element at begining of linklist

--- Link-List/Implementation of Singly Linklist.py
+++ Link-List/Implementation of Singly Linklist.py
@@ -1,16 +1,20 @@
 # ========================Singly Linklist Implementation===================
 
-class Node():
-    def __init__(self,value):
+
+    def __init__(self, value):
         self.value = value
         self.nextnode = None
 
 
-a = Node(1)
-b = Node(2)
-c = Node(3)
-a.nextnode = b
+class SLinkedList:
+    def __init__(self):
+        self.headvalue = None
+
+
+a = SLinkedList()
+a.headvalue = Node("Mon")
+b = Node("tue")
+c = Node("wed")
+a.headvalue.nextnode = b
 b.nextnode = c
-
-
-
+a.linklist()
--- Link-List/Traversing of linklist.py
+++ Link-List/Traversing of linklist.py
@@ -1,17 +1,24 @@
-class Node():
-    def __init__(self,value):
+class Node:
+    def __init__(self, value):
         self.value = value
         self.nextnode = None
 
-    def traverse(self):
-        node = self
+
+class SLinkedList:
+    def __init__(self):
+        self.headvalue = None
+
+    def linklist(self):
+        node = self.headvalue
         while node != None:
             print(node.value)
             node = node.nextnode
 
-a = Node(1)
-b = Node(2)
-c = Node(3)
-a.nextnode = b
+
+a = SLinkedList()
+a.headvalue = Node("Mon")
+b = Node("tue")
+c = Node("wed")
+a.headvalue.nextnode = b
 b.nextnode = c
-a.traverse()
+a.linklist()e()
